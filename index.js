d2f6c3afaf53a34f1b4bf8a9c38d3161bd743517
straub.ventures@gmail.com <straubfamily@Nathans-MacBook-Pro.local>
2020-10-15 12:19:56 -0700

    added image upload feature

---
+++ index.js
@@ -0,0 +1,34 @@
+//Goal: turn a piece of text into a picture.
+
+function Image(name, link, originalText, file) {
+    this.name = name,
+        this.originalText = originalText,
+        this.link = link
+}
+
+
+function uploadPic(link) {
+    var img = document.createElement("img");
+    img.src = link.value;
+    var src = document.getElementById("header");
+    src.appendChild(img);
+}
+function picrypt(userName, userText, userPicLink, file) {
+    let newObj = new Image(
+        this.name = userName,
+        this.link = userPicLink,
+        this.originalText = userText,
+        this.file = file)
+    console.log(newObj)
+}
+
+document.getElementById("form").addEventListener("submit", function (e) {
+
+    let name = document.getElementById("name")
+    let link = document.getElementById("link")
+    let text = document.getElementById("text")
+    uploadPic(link)
+    e.preventDefault()
+    picrypt(name.value, link.value, text.value, file.value)
+});
+
